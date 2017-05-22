f39279dd41a93b50424dff493c52bded83da4bc7
Nicholas Manning <nicholas.manning@gmail.com>
2017-05-22 11:51:19 +0200

    Homepage update. Removed default Vue stuff.

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,17 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        NUXT
-      </h1>
-      <h2 class="subtitle">
-        Universal Vue.js Application!
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">Github</a>
-      </div>
-    </div>
+    <h1>Nick Manning</h1>
   </section>
 </template>
 
 <script>
-import Logo from '~components/Logo.vue'
+// import Logo from '~components/Logo.vue'
 
-export default {
-  components: {
-    Logo
-  }
-}
+// export default {
+//   components: {
+//     Logo
+//   }
+// }
 </script>
 
 <style>
